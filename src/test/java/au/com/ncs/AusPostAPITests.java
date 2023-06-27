1a4698e276b1341e3e4f7a016d0e81ada59747f3
Mark Arnold <mark.arnold@accesshq.com>
2023-06-27 13:39:58 +1000

    Adds tascott Api tests

---
+++ src/test/java/au/com/ncs/AusPostAPITests.java
@@ -0,0 +1,33 @@
+package au.com.ncs;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class AusPostAPITests {
+
+    @Test
+    public void tascottStatusTest() {
+        given().
+                header("auth-key", "88737804-3df1-492c-9b97-9ad4a4bdcfe9").and().
+                param("q", "tascott").and().
+                param("state", "NSW").
+        when().
+                get("https://digitalapi.auspost.com.au/postcode/search.json").
+        then().
+                assertThat().statusCode(is(200));
+    }
+
+    @Test
+    public void tascottPostcodeTest() {
+        given().
+                header("auth-key", "88737804-3df1-492c-9b97-9ad4a4bdcfe9").and().
+                param("q", "tascott").and().
+                param("state", "NSW").
+        when().
+                get("https://digitalapi.auspost.com.au/postcode/search.json").
+        then().
+                assertThat().body("localities.locality.postcode", equalTo(2250));
+    }
+}
